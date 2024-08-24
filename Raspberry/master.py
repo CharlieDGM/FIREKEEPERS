7fa311a4ee682e692c3ed30a0e617ccd363747f2
CharlieDGM <146407126+CharlieDGM@users.noreply.github.com>
2024-08-24 12:17:48 -0600

    Mejora agrupación de objetos.
    Se agruparon en listas los diferentes objetos: variables, funciones, variables, etc.

--- Raspberry/master.py
+++ Raspberry/master.py
@@ -1,29 +1,26 @@
 #alright esta es la primera version del codigo que estoy desarrollando para la competencia
 #wro de este año, llevo ratos desarrollando pruebas asi que este solo es la union de varios
 #codigos ya desarrollados, voy a subir de forma periodica el codigo a github
-
+  
 import cv2
 import numpy as np
 #librerias para opencv/vision artifical
-
+  
 import mysql.connector
 import RPi.GPIO as GPIO
 from datetime import datetime
-#(todavia no agregado) para conectarse y actualizar datos en la db (mariaDB)
-
+#para conectarse y actualizar datos en la db (mariaDB)
+  
 import serial
 #comunicación serial
 import time
-
-fireDetected = False
-#bandera que controla acciones dependiendo si hay fuego detectado
-
+  
 class dataBase:
     def addData(ubication):
         actualDate = datetime.now()
-
+  
         temporalMark = actualDate.strftime('%d-%m-%Y %H:%M:%S')
-
+  
         conn = mysql.connector.connect(
             host="localhost", 
             user="piPython", 
@@ -36,7 +33,7 @@ class dataBase:
         conn.commit()
         conn.close()
         print(f"Se han ingresado con exito los datos", str(ubication), "y la hora: ", str(temporalMark))
-
+  
     def deleteAllData():
         conn = mysql.connector.connect(
             host="localhost", 
@@ -44,104 +41,45 @@ class dataBase:
             password="pythonistrash", 
             database="prueba01",
         )
-
+  
         cur = conn.cursor()
         cur.execute("DELETE FROM incendios;")
         conn.commit()
         conn.close()
         print("Se han eliminado todos los datos de la tabla.")
-
+  
 class camaras:
-    def camara1Lecture():
-        _, frame = cam1.read()
-        #(lo primero es para obtener un valor que no necesitamos) obtenemos una lectura de la camara
+    def camaraLecture(cam, lowerLimit, nombreVentana):
+        _, preFrame = cam.read()
+        frame = cv2.flip(preFrame, 0)
+        #(lo primero es para obtener un valor que no necesitamos xdd)
+        #obtenemos una lectura de la camara y volteamos la imagen porque las camaras estan al reves xdd
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        #convertimos los colores a HSV (Hue, Saturation, Value) para su manipulacion
-
-        lower_color = np.array([20, 40, 100])
-        upper_color = np.array([80, 255, 150])
+        #convertimos los colores a HSV el frame pasado (Hue, Saturation, Value) para su manipulacion
+  
+        lowerColor = np.array([0, 75, 50])
+        upperColor = np.array([30, 255, 255])
         #arrays de colores en hsv, nuestro margen de detección (margen de que punto a que punto vamos a detectar)
-
-        mask = cv2.inRange(hsv, lower_color, upper_color)
+  
+        mask = cv2.inRange(hsv, lowerColor, upperColor)
         #una "mascara" binaria, en la cual el 1 (blanco) representa si el pixel cumple con el rango establecido y 0 (negro) si no 
-
-        num_pixels = cv2.countNonZero(mask)
+  
+        numPixels = cv2.countNonZero(mask)
         #contamos la cantidad de pixeles en la mascara que cumplieron con el margen
-        total_pixels = mask.shape[0] * mask.shape[1]
+        totalPixels = mask.shape[0] * mask.shape[1]
         #multiplicamos el ancho por el alto para saber cuantos pixeles hay en la camara
-        percentage = (num_pixels * 100) / total_pixels
+        percentage = (numPixels * 100) / totalPixels
         #formula para obtener porcentaje
-
-        #cv2.imshow('Original', frame)
-        cv2.imshow('Mask', mask)
-        if percentage >= 15:
+  
+        #creamos una ventana para poder mostrarla xdd
+        cv2.namedWindow(nombreVentana, cv2.WINDOW_NORMAL)
+        cv2.imshow(nombreVentana, mask)
+          
+        if percentage >= lowerLimit:
             return True
         else:
-            return False
-    
-    def camara2Lecture():
-        _, frame = cam2.read()
-        #(lo primero es para obtener un valor que no necesitamos) obtenemos una lectura de la camara
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        #convertimos los colores a HSV (Hue, Saturation, Value) para su manipulacion
-
-        lower_color = np.array([40, 50, 50])
-        upper_color = np.array([80, 255, 255])
-
-        mask = cv2.inRange(hsv, lower_color, upper_color) 
-
-        num_pixels = cv2.countNonZero(mask)
-        total_pixels = mask.shape[0] * mask.shape[1]
-        percentage = (num_pixels * 100) / total_pixels
-
-        cv2.imshow('Mask', mask)
-        if percentage >= 20:
-            return True
-        else:
-            return False
-    
-    def camara3Lecture():
-        _, frame = cam3.read()
-        #(lo primero es para obtener un valor que no necesitamos) obtenemos una lectura de la camara
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        #convertimos los colores a HSV (Hue, Saturation, Value) para su manipulacion
-
-        lower_color = np.array([40, 50, 50])
-        upper_color = np.array([80, 255, 255])
-
-        mask = cv2.inRange(hsv, lower_color, upper_color) 
-
-        num_pixels = cv2.countNonZero(mask)
-        total_pixels = mask.shape[0] * mask.shape[1]
-        percentage = (num_pixels * 100) / total_pixels
-
-        cv2.imshow('Mask', mask)
-        if percentage >= 20:
-            return True
-        else:
-            return False
-    
-    def camara4Lecture():
-        _, frame = cam4.read()
-        #(lo primero es para obtener un valor que no necesitamos) obtenemos una lectura de la camara
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        #convertimos los colores a HSV (Hue, Saturation, Value) para su manipulacion
-
-        lower_color = np.array([40, 50, 50])
-        upper_color = np.array([80, 255, 255])
-
-        mask = cv2.inRange(hsv, lower_color, upper_color) 
-
-        num_pixels = cv2.countNonZero(mask)
-        total_pixels = mask.shape[0] * mask.shape[1]
-        percentage = (num_pixels * 100) / total_pixels
-
-        cv2.imshow('Mask', mask)
-        if percentage >= 20:
-            return True
-        else:
-            return False
-
+              return False
+  
 def serialLecture():
     if serial.in_waiting > 0:
         #serial.reset_input_buffer()
@@ -150,7 +88,7 @@ def serialLecture():
         #por partes: se lee la linea recibida, se decodifica y se limpian los espacios en blanco
         print(mensaje)
         return mensaje
-            
+              
 def serialSend(mensajeToSend):
     if type(mensajeToSend) != str:
         str(mensajeToSend)
@@ -163,45 +101,35 @@ def serialSend(mensajeToSend):
     time.sleep(0.003)
     serial.write(mensaje.encode('utf-8'))
     print(f"Enviado: {mensajeToSend}")
-        
+          
 if __name__ == '__main__':
     alreadyActivated = False
-    
+      
     cam1 = cv2.VideoCapture(0)
-    #cam2 = cv2.VideoCapture(2)
-    #cam3 = cv2.VideoCapture(4)
-    #cam4 = cv2.VideoCapture(6)
-    
+    cam2 = cv2.VideoCapture(2)
+    cam3 = cv2.VideoCapture(4)
+      
+    cams = [cam1, cam2, cam3]
+    nombreVentanas = ['cam1', 'cam2', 'cam3']
+      
     serial = serial.Serial(
         port='/dev/ttyS0', #ttyS0 son los pines RX y TX, el puerto de conexión puede variar
-        baudrate=9600, #no cambiar, esta sincronizado con el arduino
+        baudrate=9600, #no cambiar, esta sincronizado con el ESP32
         timeout=2
     ) #datos de conección UART
     serial.flush()
     #se limpia todo mensaje que exista en ese momento
     try:
         while True:
-            if fireDetected == False:
-                if serialLecture() == "norte" or camaras.camara1Lecture() == True:
-                    fireDetected = True
-            else:
-                if alreadyActivated == False:
-                    serialSend("norte")
-                    dataBase.addData("norte")
-                    alreadyActivated = True
-                if serialLecture() == "reset":
-                        fireDetected = False
-                        alreadyActivated = False
-                        dataBase.deleteAllData
-            
+            for n in range(3):
+                if camaras.camaraLecture(cams[n], 15, nombreVentanas[n]):
+                    print(f"Camara {n+1} True.")
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                dataBase.deleteAllData()
                 break
     finally:
-        cam1.release()
-        #cam2.release()
-        #cam3.release()
-        #cam4.release()
-        
-        cv2.destroyAllWindows()
-        
+        for cam in cams:
+            cam.release()
+                  
+        cv2.destroyAllWindows()  
         serial.close()
