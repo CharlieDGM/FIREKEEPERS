234b1592b36732bb787c1ea5994cdda0af71888f
CharlieDGM <146407126+CharlieDGM@users.noreply.github.com>
2024-08-26 16:09:35 -0600

    reestructura de comunicacion
    Se cambio el modo en el cual el esp32 se comunica con la raspberry. Ahora utiliza un servidor para poder funcionar forma correcta y mejor.

--- ESP32/master.ino
+++ ESP32/master.ino
@@ -3,21 +3,17 @@
 //va a ancender las baterias(o fuentes) de los motores y va a mandar señales PWM a los controladores
 //de los motores. Mandara mensajes a la Raspberry para poder operar en conjunto y realizar tareas
 //especificas
-
-#include <HardwareSerial.h>
-//no tengo idea si esto es necesario para imprimir mensajes en la consola, lo voy a dejar por si las moscas xd
+#include <WiFi.h>
 #include <ESP32Servo.h>
 //librerias iniciales
 
-HardwareSerial rpi(1); // PUERTO UART 2 / / conectado a pines 16 y 17 en el setup / objeto que se usara para la comunicacion serial RPI - ESP32
+WiFiServer server(80); //Servidor local en el puerto 80. Utilizado para conectarse con la Raspberry
+
 int sensorPins[3] = {32, 26, 12};
 //arrays que contienen a donde van conectados los componentes
-//int relayPins[4] = {2, 3, 4, 5}; ELIMINAR O CAMBIAR PINOUT DE LOS RELAY EN FUTURAS VERSIONES
 Servo escControl[4]; //23, 21, 18, 2 (se les coloca en esos pines en el setup)
 
-//bool FireState = false; //ELIMINAR TODA INSTANCIA DE ESTA BANDERA EN FUTURAS VERSIONES
-
-bool AnalogLecture(char InPin, int limit) {
+bool AnalogLecture(int InPin, int limit) {
   int aux = analogRead(InPin); //lee la entrada del pin analógico que le indiquemos
   if (aux > limit) {
     Serial.println("Pin Leido: " + String(InPin) + ". Lectura Obtenida: " + String(aux));
@@ -26,17 +22,6 @@ bool AnalogLecture(char InPin, int limit) {
     Serial.println("Pin Leido: " + String(InPin) + ". Lectura Obtenida: " + String(aux));
     return false;
   } //devuelve true o false dependiendo de la lectura
-}
-
-String rpiReading() {
-  String lecture;
-
-  if (rpi.available()) {  //comprueba si hay datos seriales disponibles4
-  lecture = rpi.readStringUntil('\n');
-    lecture.trim(); //ajustamos la lectura por cualquier error que pueda ocurrir
-    Serial.println("Lectura de la raspberry lol xd: " + lecture);
-    return lecture; //imprimimos y guardamos el valor de la lectura obtenida
-  }
 }
 
 class Motor { //una clase que contiene funciones para encender y apagar las bombas de agua. Necesitamos controlar un relay y mandar una señal a los controladores
@@ -49,9 +34,8 @@ class Motor { //una clase que contiene funciones para encender y apagar las bomb
   }
 
   void On(int pulseToSend) {
-    //digitalWrite(relayPins[motorNumber], HIGH); A;ADIR SI SE MODIFICA LA LINEA 15
     escControl[motorNumber].write(pulseToSend); //encendemos el relay junto con el controlador del motor
-    Serial.println("Se ha encendido el motor: " + String(motorNumber+1) + ". Pulso enviado: " + String(pulseToSend));
+    Serial.println("Motor: " + String(motorNumber+1) + ". Pulso enviado: " + String(pulseToSend));
   }
 
   void Off() {
@@ -63,35 +47,51 @@ class Motor { //una clase que contiene funciones para encender y apagar las bomb
 Motor motors[4] = {Motor(0), Motor(1), Motor(2), Motor(3)};
 
 void setup() {
-  Serial.begin(9600);
-  rpi.begin(9600, SERIAL_8N1, 16, 17); 
-  //iniciamos la comunicación serial de estas cosas todas horribles (conectado al 2do puerto: 16 rx, 17 tx)
+  Serial.begin(9600); 
+  //iniciamos la comunicación serial de estas cosas todas horribles (conectado al 2do puerto: 16 (cable negro) rx, 17 (cable blanco) tx)
 
-  char escControlPins[4] = {23, 21, 18, 2}; //a la izquierda del controlador
+  char escControlPins[4] = {23, 21, 18, 2}; //a la izquierda del controlador (Motores A, B, C, D)
   delay(2000);
   Serial.println("");
   Serial.println("Encendiendo...");
   delay(2000);
-  rpi.println("start");
   for (int n = 0; n < 4; n++) { //aprovechando que de todo tenemos 4 metemos todo en un ciclo for para iniciar sus protocolos correspondientes  
-    Serial.println("Se ha iniciado el motor: " + String(n+1));
+    Serial.println("Encendiendo el motor: " + String(n+1));
     escControl[n].attach(escControlPins[n], 1000, 2000);
     escControl[n].write(0);
     delay(1000);
-
-    //pinMode(relayPins[n], OUTPUT); AÑADIR SOLAMENTE SI SE MODIFICA LA LINEA 15
-    //lo mismo, iniciamos los pines de los sensores y los relays
   }
 
   for (int n = 0; n < 3; n++) {
     pinMode(sensorPins[n], INPUT); //pero sensores solo son 3 >:/ (asi q hay que hacer otro ciclo for)
   }
-  Serial.println("Iniciando todo pipipip...");
+
+  WiFi.begin("CLARO_193EC7", "22FC83B7F3"); //Nos conectamos al internet con las credenciales de la red
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000); //Esperamos que se realice la conexion a la red.
+    Serial.println("Espera wachin, me estoy conectando al internet...");
+  }
+  Serial.println("Ya me conecte al internet xd");
+
+  server.begin(); //iniciamos el servidor local
+  Serial.println("Servidor iniciado");
+  Serial.print("Direccion IP del servidor: ");
+  Serial.println(WiFi.localIP()); //no tengo idea porque no me deja concatenar esta linea con la anterior.
+  delay(2000);
+
+  Serial.println("Esperando conexion del cliente/raspberryPi xddd...");
+  while (!server.available()) {
+    delay(750);
+    Serial.print("."); //Esperamos a que se conecte la raspberry pi.
+  }
+  Serial.println("\nSe ha conectado el cliente.\n");
+
+  Serial.println("=======================================================");
+  Serial.println("Iniciando todo pipipipipi... (3.14 3.14 3.14 3.14 3.14)");
   delay(3000);
 }
 
 void loop() {
-  motors[0].On(100);
   //REESTRUCTURAR COMPLETAMENTE EL ALGORITMO
 } 
 
