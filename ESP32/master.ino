0bb962a2394aadbeb81ddc9527f2480012498ac7
CharlieDGM <146407126+CharlieDGM@users.noreply.github.com>
2024-09-27 23:33:12 -0600

    traduccion de mensajes y variables

--- ESP32/master.ino
+++ ESP32/master.ino
@@ -12,7 +12,7 @@ WiFiServer server(80); //Servidor local en el puerto 80. Utilizado para conectar
 int sensorPins[3] = {32,26,14}; // sensores 1, 2, 3
 //arrays que contienen a donde van conectados los componentes
 Servo escControl[4]; //23, 21, 18, 2 (se les coloca en esos pines en el setup)
-int pulsos[4] = {50, 75, 70, 60}; //pulsos de las bombas: A, B, C, D
+int pulsos[4] = {60, 75, 70, 65}; //pulsos de las bombas: A, B, C, D
 
 bool AnalogLecture(int InPin, int limit, bool inverted) {
   int aux = analogRead(InPin); 
@@ -31,7 +31,7 @@ class Motor { //una clase que contiene funciones para encender y apagar las bomb
 
   void On(int pulseToSend) {
     escControl[motorNumber].write(pulseToSend); //encendemos el relay junto con el controlador del motor
-    Serial.println("Motor: " + String(motorNumber+1) + ". Pulso enviado: " + String(pulseToSend));
+    Serial.println("Motor: " + String(motorNumber+1) + ". PWM sent: " + String(pulseToSend));
   }
 
   void Off() {
@@ -81,85 +81,85 @@ void setup() {
   int escControlPins[4] = {23, 21, 18, 2}; //a la izquierda del controlador (Motores A, B, C, D)
   delay(2000);
   Serial.println("");
-  Serial.println("Encendiendo...");
+  Serial.println("Starting the system...");
   delay(4000);
   for (int n = 0; n < 4; n++) { //aprovechando que de todo tenemos 4 metemos todo en un ciclo for para iniciar sus protocolos correspondientes  
-    Serial.println("Encendiendo el motor: " + String(n+1));
+    Serial.println("Turning on motor " + String(n+1) + "...");
     escControl[n].attach(escControlPins[n], 1000, 2000);
     escControl[n].write(0);
     delay(3000);
   }
   escControl[2].write(0);
-  Serial.println("Re-encendiendo el tercer motor...");
+  Serial.println("Turning on motor 3 again... (dont ask why, it just works)");
   delay(3000);
 
   for (int n = 0; n < 3; n++) {
     pinMode(sensorPins[n], INPUT);
     int lecturaInicial = analogRead(sensorPins[n]); //pero sensores solo son 3 >:/ (asi q hay que hacer otro ciclo for)
-    Serial.println("Sensor iniciado: " + String(n+1) + ". Lectura inicial: " + String(lecturaInicial));
+    Serial.println("Sensor started: " + String(n+1) + ". Turn-on lecture: " + String(lecturaInicial));
     delay(500);
   }
 
   WiFi.begin("CLARO_193EC7", "22FC83B7F3"); //Nos conectamos al internet con las credenciales de la red
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000); //Esperamos que se realice la conexion a la red.
-    Serial.println("Espera wachin, me estoy conectando al internet...");
+    Serial.println("wait, im conecting to the internet...");
   }
-  Serial.println("Ya me conecte al internet xd");
+  Serial.println("i have conected to the internet lol");
 
   server.begin(); //iniciamos el servidor local
-  Serial.println("Servidor iniciado");
-  Serial.print("Direccion IP del servidor: ");
+  Serial.println("Servidor started");
+  Serial.print("IP ADDRESS: ");
   Serial.println(WiFi.localIP()); //no tengo idea porque no me deja concatenar esta linea con la anterior.
   delay(2000);
 
-  Serial.println("Esperando conexion del cliente/raspberryPi xddd...");
+  Serial.println("Waiting conection from the RaspberryPI...");
   while (!server.available()) {
     delay(750);
     Serial.print("."); //Esperamos a que se conecte la raspberry pi.
   }
-  Serial.println("\nSe ha conectado el cliente.\n");
+  Serial.println("\nClient Conected\n");
 
-  Serial.println("=======================================================");
-  Serial.println("Iniciando todo pipipipipi... (3.14 3.14 3.14 3.14 3.14)");
+  Serial.println("===============================================================");
+  Serial.println("Starting FIREKEEPERS pipipipipipi... (3.14 3.14 3.14 3.14 3.14)");
 }
 
-String mensajesRPI[3] = {"derecha", "frente", "izquierda"}; 
+String mensajesRPI[3] = {"right", "front", "left"}; 
 
 void loop() {
   WiFiClient cliente = server.available(); // Acepta una conexión del cliente
   
   if (cliente) { // Verifica si hay un cliente conectado
-    Serial.println("Cliente conectado.");
+    Serial.println("Client connected.");
     while (cliente.connected()) { // Mientras el cliente esté conectado
     String mensaje;
       if (cliente.available()) { // Si hay datos disponibles del cliente
         mensaje = cliente.readStringUntil('\n');
-        Serial.println("Mensaje recibido: " + mensaje);
+        Serial.println("Message recieved: " + mensaje);
         cliente.flush(); // Limpia la entrada del cliente para evitar sobrecarga
       }
       
       int lecturaSensor = analogRead(sensorPins[0]);
       Serial.println(lecturaSensor);
       if (controlarMotores(lecturaSensor)) {
-        cliente.println("encendido");
+        cliente.println("turnOn");
         delay(3000);
       } else { //dependiendo de las diferentes lecturas mandamos los datos al Raspberry y encendemos o apagamos los motores
-        cliente.println("apagado");
+        cliente.println("turnOff");
         delay(1000);
       }
 
       for (int n = 0; n < 3; n++) {
         if (mensaje == mensajesRPI[n]) {
           encender(n); 
-          Serial.println("Se ha detectado fuego en la camara: " + String(n+1));
+          Serial.println("Fire detected in the camara " + String(n+1) + ".");
           delay(6000);
         }
       }
     }
     
     // Cuando se desconecta el cliente
-    Serial.println("Cliente desconectado.");
+    Serial.println("Client disconected.");
     cliente.stop(); // Cierra la conexión del cliente adecuadamente
   }
 }
